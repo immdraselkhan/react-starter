fd2f694388c57c79948b9f3851885fe638a1c524
Md Rasel Khan <immdraselkhan@gmail.com>
2022-10-16 02:28:30 +0600

    initial release

---
+++ src/routes/routes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Main from '../layouts/Main';
+import Home from '../components/Home';
+import Error from '../components/Error';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    errorElement: <Error />,
+    element: <Main />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+    ],
+  },
+]);
